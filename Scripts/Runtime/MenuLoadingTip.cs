401e308493edead2e33ae5000243219dd0311757
Vulpes Software <tylahheil@gmail.com>
2020-08-14 14:17:13 +1000

    ADD: Added MenuLoading screen class and MenuLoadingTip scriptable object.

---
+++ Scripts/Runtime/MenuLoadingTip.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Vulpes.Menus
+{
+    [CreateAssetMenu(fileName = "New Menu Loading Tip", menuName = "Vulpes/Menus/Menu Loading Tip", order = 4096)]
+    public sealed class MenuLoadingTip : ScriptableObject
+    {
+        public string title;
+        public string subtitle;
+        public string body;
+        public Texture texture;
+    }
+}
